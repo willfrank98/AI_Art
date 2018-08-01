582fb8739989e986020093c539ebe9945f705026
Will Frank <willfrank98@gmail.com>
2018-07-31 21:30:14 -0600

    yeet

--- AI_Art/Program.cs
+++ AI_Art/Program.cs
@@ -1,17 +1,18 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Drawing;
 
 namespace AI_Art
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-            Console.ReadKey();
-        }
-    }
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.Write("Enter a random seed: ");
+			var seed = Console.ReadLine();
+			var gen = new ImageGenerator();
+			var image = gen.DrawTextImage("sample text", new Font(FontFamily.GenericMonospace, 12), Color.Black, Color.White);
+			image.Save("image.jpg", System.Drawing.Imaging.ImageFormat.Jpeg);
+			image.Dispose();
+		}
+	}
 }
