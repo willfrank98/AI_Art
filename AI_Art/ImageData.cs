1184061f3c1c4e927e19054623fc4378e2f220d6
Will Frank <willfrank98@gmail.com>
2018-08-16 02:15:09 -0600

    shit broke

--- AI_Art/ImageData.cs
+++ AI_Art/ImageData.cs
@@ -7,8 +7,7 @@ namespace AI_Art
 {
 	internal class ImageData
 	{
-		private readonly int MAX_LENGTH = 200;
-		private readonly int MIN_LENGTH = 100;
+
 		private Triangle[] _triangles;
 		private int _imageNumber;
 
@@ -24,53 +23,11 @@ namespace AI_Art
 			_triangles = new Triangle[numberOfTriangles];
 			for (int i = 0; i < _triangles.Length; i++)
 			{
-				var tempX1 = rand.Next(0, 1920);
-				var tempY1 = rand.Next(0, 1920);
-				var point1 = new Point(tempX1, tempY1);
-
-				var tempX2 = rand.Next((MAX_LENGTH - MIN_LENGTH) * 2);
-				if (tempX2 < MAX_LENGTH - MIN_LENGTH)
-				{
-					tempX2 = tempX1 - MIN_LENGTH - tempX2;
-				}
-				else
-				{
-					tempX2 = tempX1 + MIN_LENGTH + tempX2;
-				}
-				var tempY2 = rand.Next((MAX_LENGTH - MIN_LENGTH) * 2);
-				if (tempY2 < MAX_LENGTH - MIN_LENGTH)
-				{
-					tempY2 = tempY1 - MIN_LENGTH - tempY2;
-				}
-				else
-				{
-					tempY2 = tempY1 + MIN_LENGTH + tempY2;
-				}
-				var point2 = new Point(tempX2, tempY2);
-
-				var tempX3 = rand.Next((MAX_LENGTH - MIN_LENGTH) * 3);
-				if (tempX3 < MAX_LENGTH - MIN_LENGTH)
-				{
-					tempX3 = tempX1 - MIN_LENGTH - tempX3;
-				}
-				else
-				{
-					tempX3 = tempX1 + MIN_LENGTH + tempX3;
-				}
-				var tempY3 = rand.Next((MAX_LENGTH - MIN_LENGTH) * 3);
-				if (tempY3 < MAX_LENGTH - MIN_LENGTH)
-				{
-					tempY3 = tempY1 - MIN_LENGTH - tempY3;
-				}
-				else
-				{
-					tempY3 = tempY1 + MIN_LENGTH + tempY3;
-				}
-				var point3 = new Point(tempX3, tempY3);
+				var newPoints = Triangle.MakeThreePoints();
 
 				var brush = new SolidBrush(Color.FromArgb(rand.Next(256), rand.Next(256), rand.Next(256)));
 
-				_triangles[i] = new Triangle(new Point[] { point1, point2, point3}, brush);
+				_triangles[i] = new Triangle(newPoints, brush);
 			}
 
 			_image = new Bitmap(width, height, PixelFormat.Format24bppRgb);
--- AI_Art/Triangle.cs
+++ AI_Art/Triangle.cs
@@ -8,6 +8,8 @@ namespace AI_Art
 {
 	class Triangle
 	{
+		private static int MAX_LENGTH = 200;
+		private static int MIN_LENGTH = 100;
 		public Point[] _points;
 		public Brush _brush;
 
@@ -19,25 +21,64 @@ namespace AI_Art
 
 		public Triangle()
 		{
-			var rand = new Random(DateTime.Now.Millisecond);
-			var MAX_LENGTH = 500;
+			Random rand = new Random(DateTime.Now.Millisecond);
+			var newPoints = MakeThreePoints();
 
+			var brush = new SolidBrush(Color.FromArgb(rand.Next(256), rand.Next(256), rand.Next(256)));
+
+			_points = newPoints;
+			_brush = brush;
+		}
+
+		public static Point[] MakeThreePoints()
+		{
+			Random rand = new Random(DateTime.Now.Millisecond);
 			var tempX1 = rand.Next(0, 1920);
 			var tempY1 = rand.Next(0, 1920);
 			var point1 = new Point(tempX1, tempY1);
 
-			var tempX2 = rand.Next(tempX1 - MAX_LENGTH, tempX1 + MAX_LENGTH);
-			var tempY2 = rand.Next(tempY1 - MAX_LENGTH, tempY1 + MAX_LENGTH);
+			var tempX2 = rand.Next((MAX_LENGTH - MIN_LENGTH) * 2);
+			if (tempX2 < MAX_LENGTH - MIN_LENGTH)
+			{
+				tempX2 = tempX1 - MIN_LENGTH - tempX2;
+			}
+			else
+			{
+				tempX2 = tempX1 + MIN_LENGTH + tempX2;
+			}
+			var tempY2 = rand.Next((MAX_LENGTH - MIN_LENGTH) * 2);
+			if (tempY2 < MAX_LENGTH - MIN_LENGTH)
+			{
+				tempY2 = tempY1 - MIN_LENGTH - tempY2;
+			}
+			else
+			{
+				tempY2 = tempY1 + MIN_LENGTH + tempY2;
+			}
 			var point2 = new Point(tempX2, tempY2);
 
-			var tempX3 = rand.Next(tempX1 - MAX_LENGTH, tempX1 + MAX_LENGTH);
-			var tempY3 = rand.Next(tempY1 - MAX_LENGTH, tempY1 + MAX_LENGTH);
+			var tempX3 = rand.Next((MAX_LENGTH - MIN_LENGTH) * 3);
+			if (tempX3 < MAX_LENGTH - MIN_LENGTH)
+			{
+				tempX3 = tempX1 - MIN_LENGTH - tempX3;
+			}
+			else
+			{
+				tempX3 = tempX1 + MIN_LENGTH + tempX3;
+			}
+			var tempY3 = rand.Next((MAX_LENGTH - MIN_LENGTH) * 3);
+			if (tempY3 < MAX_LENGTH - MIN_LENGTH)
+			{
+				tempY3 = tempY1 - MIN_LENGTH - tempY3;
+			}
+			else
+			{
+				tempY3 = tempY1 + MIN_LENGTH + tempY3;
+			}
 			var point3 = new Point(tempX3, tempY3);
 
-			var brush = new SolidBrush(Color.FromArgb(rand.Next(256), rand.Next(256), rand.Next(256)));
-
-			_points = new Point[] { point1, point2, point3 };
-			_brush = brush;
+			var points = new Point[] { point1, point2, point3 };
+			return points;
 		}
 	}
 }
