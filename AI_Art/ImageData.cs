d85671619f9db77aa76e438bd6fa4e8678c4a0e5
Will Frank <willfrank98@gmail.com>
2018-08-16 01:58:31 -0600

    whoops

--- AI_Art/ImageData.cs
+++ AI_Art/ImageData.cs
@@ -31,40 +31,40 @@ namespace AI_Art
 				var tempX2 = rand.Next((MAX_LENGTH - MIN_LENGTH) * 2);
 				if (tempX2 < MAX_LENGTH - MIN_LENGTH)
 				{
-					tempX2 = 0 - MIN_LENGTH - tempX2;
+					tempX2 = tempX1 - MIN_LENGTH - tempX2;
 				}
 				else
 				{
-					tempX2 = 0 + MIN_LENGTH + tempX2;
+					tempX2 = tempX1 + MIN_LENGTH + tempX2;
 				}
 				var tempY2 = rand.Next((MAX_LENGTH - MIN_LENGTH) * 2);
 				if (tempY2 < MAX_LENGTH - MIN_LENGTH)
 				{
-					tempY2 = 0 - MIN_LENGTH - tempY2;
+					tempY2 = tempY1 - MIN_LENGTH - tempY2;
 				}
 				else
 				{
-					tempY2 = 0 + MIN_LENGTH + tempY2;
+					tempY2 = tempY1 + MIN_LENGTH + tempY2;
 				}
 				var point2 = new Point(tempX2, tempY2);
 
 				var tempX3 = rand.Next((MAX_LENGTH - MIN_LENGTH) * 3);
 				if (tempX3 < MAX_LENGTH - MIN_LENGTH)
 				{
-					tempX3 = 0 - MIN_LENGTH - tempX3;
+					tempX3 = tempX1 - MIN_LENGTH - tempX3;
 				}
 				else
 				{
-					tempX3 = 0 + MIN_LENGTH + tempX3;
+					tempX3 = tempX1 + MIN_LENGTH + tempX3;
 				}
 				var tempY3 = rand.Next((MAX_LENGTH - MIN_LENGTH) * 3);
 				if (tempY3 < MAX_LENGTH - MIN_LENGTH)
 				{
-					tempY3 = 0 - MIN_LENGTH - tempY3;
+					tempY3 = tempY1 - MIN_LENGTH - tempY3;
 				}
 				else
 				{
-					tempY3 = 0 + MIN_LENGTH + tempY3;
+					tempY3 = tempY1 + MIN_LENGTH + tempY3;
 				}
 				var point3 = new Point(tempX3, tempY3);
 
