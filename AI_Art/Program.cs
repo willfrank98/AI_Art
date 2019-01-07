d6f07b4df4b4e8893d00c8269a5c0c5b505f7022
Will Frank <willfrank98@gmail.com>
2019-01-06 17:39:54 -0700

    progress has been made

--- AI_Art/Program.cs
+++ AI_Art/Program.cs
@@ -1,23 +1,128 @@
 using System;
-using System.Drawing;
+using System.Diagnostics;
 
 namespace AI_Art
 {
 	class Program
 	{
-		static void Main(string[] args)
+		static int Main(string[] args)
 		{
-			//Console.Write("Enter a random seed: ");
-			var seed = /*Console.ReadLine();*/DateTime.Now.Millisecond.GetHashCode();/*"Will"*/;
-			//Console.Write("How many images would you like to generate at once?: ");
-			var num = 1000000;
-			var filepath = "MonaLisa.jpg";
+			string fileIn = "";
+			string fileOut = "";
+			int num = 0;
+			int minLength = 0;
+			int maxLength = 0;
+			int granularity = 0;
+			int drawNum = 0;
+			int seed = 0;
+			if (args.Length == 0)
+			{
+				Console.WriteLine("Enter input file: ");
+				fileIn = Console.ReadLine();
+				Console.WriteLine("Enter output file: ");
+				fileOut = Console.ReadLine();
+				Console.WriteLine("Enter number of triangles to generate: ");
+				if (!int.TryParse(Console.ReadLine(), out num))
+				{
+					Console.WriteLine("Invalid input!");
+					return 1;
+				}
+				Console.WriteLine("Enter minimum side length: ");
+				if (!int.TryParse(args[3], out minLength))
+				{
+					Console.WriteLine("Invalid input!");
+					return 1;
+				}
+				Console.WriteLine("Enter maximum side length: ");
+				if (!int.TryParse(args[4], out maxLength))
+				{
+					Console.WriteLine("Invalid input!");
+					return 1;
+				}
+				Console.WriteLine("Enter granularity level: ");
+				if (!int.TryParse(args[5], out granularity))
+				{
+					Console.WriteLine("Invalid input!");
+					return 1;
+				}
+				Console.WriteLine("Enter number of triangles to generate: ");
+				if (!int.TryParse(args[6], out drawNum))
+				{
+					Console.WriteLine("Invalid input!");
+					return 1;
+				}
+			}
+			else if (args.Length >= 1)
+			{
+				if (string.Equals(args[0], "-hc"))
+				{
+					args = new string[8];
+					args[0] = "MonaLisa.jpg";   //source file
+					args[1] = "image.bmp";      //destination file
+					args[2] = "2000000";        //number to generate
+					args[3] = "5";              //min side length
+					args[4] = "30";             //max side length
+					args[5] = "3";              //granularity to evaluate at
+					args[6] = "50000";          //number to draw
+					args[7] = DateTime.Now.Millisecond.ToString(); //random seed
+				}
+				else
+				{
+					Console.WriteLine("Invalid Args!");
+				}
 
-			var t = new TriangleImage(seed, filepath);
+				fileIn = args[0];
+				fileOut = args[1];
 
-			t.NewBatch(num, 10, 30);
-			t.EvaluateFitness();
-			t.Draw(100000);
+				if (!int.TryParse(args[2], out num))
+				{
+					Console.WriteLine("Invalid Args!");
+					return 1;
+				}
+
+				if (!int.TryParse(args[3], out minLength))
+				{
+					Console.WriteLine("Invalid Args!");
+					return 1;
+				}
+
+				if (!int.TryParse(args[4], out maxLength))
+				{
+					Console.WriteLine("Invalid Args!");
+					return 1;
+				}
+
+				if (!int.TryParse(args[5], out granularity))
+				{
+					Console.WriteLine("Invalid Args!");
+					return 1;
+				}
+
+				if (!int.TryParse(args[6], out drawNum))
+				{
+					Console.WriteLine("Invalid Args!");
+					return 1;
+				}
+
+				seed = args[7].GetHashCode();
+			}
+			
+
+			var t = new TriangleImage(seed, fileIn);
+
+			Stopwatch timer = new Stopwatch();
+			timer.Start();
+
+			t.NewBatch(num, minLength, maxLength);
+			t.EvaluateFitnessExactMatch(granularity);
+			t.Draw(drawNum, fileOut);
+
+			timer.Stop();
+
+			TimeSpan ts = timer.Elapsed;
+			string elapsedTime = String.Format("{0:00}:{1:00}:{2:00}.{3:00}", ts.Hours, ts.Minutes, ts.Seconds,	ts.Milliseconds / 10);
+			Console.WriteLine($"Finished in {elapsedTime}.");
+			return 0;
 		}
 	}
 }
--- AI_Art/TriangleImage.cs
+++ AI_Art/TriangleImage.cs
@@ -2,9 +2,6 @@
 using System.Collections.Generic;
 using System.Drawing;
 using System.Drawing.Imaging;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace AI_Art
 {
@@ -30,13 +27,21 @@ namespace AI_Art
 		public void NewBatch(int num, int minLength, int maxLength)
 		{
 			tris = new Triangle[num];
+			int onePercent = num / 100;
 			for (int i = 0; i < num; i++)
 			{
 				tris[i] = new Triangle(rand, height, width, minLength, maxLength);
+
+				if (i % onePercent == 0)
+				{
+					decimal percent = decimal.Divide(i, tris.Length) * 100;
+					Console.SetCursorPosition(0, 0);
+					Console.WriteLine("Generating: {0:0}%", percent);
+				}
 			}
 		}
 
-		public unsafe void EvaluateFitness()
+		public unsafe void EvaluateFitnessExactMatch(int granularity)
 		{
 			//open image
 			Bitmap b = new Bitmap(image);
@@ -49,11 +54,11 @@ namespace AI_Art
 			//evaluate triangles
 			double[] fitness = new double[tris.Length];
 
-			//int i = 0;
-			//foreach (Triangle triangle in tris.AsParallel())
+			int onePercent = tris.Length / 100;
 			for (int i = 0; i < tris.Length; i++)
 			{
-				foreach (Point point in PointsInTriangle(tris[i]._points[0], tris[i]._points[1], tris[i]._points[2]))
+				int pixels = 0;
+				foreach (Point point in PointsInTriangle(tris[i]._points[0], tris[i]._points[1], tris[i]._points[2], granularity))
 				{
 					if (point.X >= 0 && point.X < image.Width && point.Y >= 0 && point.Y < image.Height)
 					{
@@ -61,17 +66,22 @@ namespace AI_Art
 
 						//higher fitness = better
 						Color color = ((SolidBrush)tris[i]._brush).Color;
-						fitness[i] += Math.Pow(1.02, 255 - Math.Abs(data[0] - color.B));
-						fitness[i] += Math.Pow(1.02, 255 - Math.Abs(data[1] - color.R));
-						fitness[i] += Math.Pow(1.02, 255 - Math.Abs(data[2] - color.G));
+						fitness[i] += Math.Pow(1.03, 255 - Math.Abs(data[0] - color.B));
+						fitness[i] += Math.Pow(1.03, 255 - Math.Abs(data[1] - color.R));
+						fitness[i] += Math.Pow(1.03, 255 - Math.Abs(data[2] - color.G));
+						pixels++;
 					}
 				}
 
+				fitness[i] = fitness[i] / Math.Pow(pixels, .5);
+
 				tris[i]._fitness = fitness[i];
 
-				if (i % 1000 == 0)
+				if (i % onePercent == 0)
 				{
-					Console.WriteLine($"Doing: {i}");
+					decimal percent = decimal.Divide(i, tris.Length) * 100;
+					Console.SetCursorPosition(0, 1);
+					Console.WriteLine("Evaluating: {0:0}%", percent);
 				}
 			}
 
@@ -79,11 +89,75 @@ namespace AI_Art
 			Array.Sort(fitness, tris);
 		}
 
+		//public unsafe void EvaluateFitnessLowVarience()
+		//{
+		//	//open image
+		//	Bitmap b = new Bitmap(image);
+		//	BitmapData bData = b.LockBits(new Rectangle(0, 0, image.Width, image.Height), ImageLockMode.ReadWrite, b.PixelFormat);
+
+		//	byte bitsPerPixel = GetBitsPerPixel(bData.PixelFormat);
+
+		//	byte* scanImage = (byte*)bData.Scan0.ToPointer();
+
+		//	//evaluate triangles
+		//	double[] fitness = new double[tris.Length];
+
+		//	//int i = 0;
+		//	//foreach (Triangle triangle in tris.AsParallel())
+		//	for (int i = 0; i < tris.Length; i++)
+		//	{
+		//		int totalDiff = 0;
+		//		int n = 0;
+		//		foreach (Point point in PointsInTriangle(tris[i]._points[0], tris[i]._points[1], tris[i]._points[2]))
+		//		{
+		//			if (point.X >= 0 && point.X < image.Width && point.Y >= 0 && point.Y < image.Height)
+		//			{
+		//				byte* data = scanImage + point.Y * bData.Stride + point.X * bitsPerPixel / 8;
+
+		//				Color color = ((SolidBrush)tris[i]._brush).Color;
+		//				totalDiff += 255 - Math.Abs(data[0] - color.B);
+		//				totalDiff += 255 - Math.Abs(data[1] - color.R);
+		//				totalDiff += 255 - Math.Abs(data[2] - color.G);
+		//				n++;
+		//			}
+		//		}
+
+		//		double avgDiff = (double)totalDiff / (double)n;
+
+		//		foreach (Point point in PointsInTriangle(tris[i]._points[0], tris[i]._points[1], tris[i]._points[2], ))
+		//		{
+		//			if (point.X >= 0 && point.X < image.Width && point.Y >= 0 && point.Y < image.Height)
+		//			{
+		//				byte* data = scanImage + point.Y * bData.Stride + point.X * bitsPerPixel / 8;
+
+		//				Color color = ((SolidBrush)tris[i]._brush).Color;
+		//				double tempFit = 0;
+		//				tempFit += 255 - Math.Abs(data[0] - color.B);
+		//				tempFit += 255 - Math.Abs(data[1] - color.R);
+		//				tempFit += 255 - Math.Abs(data[2] - color.G);
+		//				fitness[i] = -Math.Abs(avgDiff - tempFit);
+		//			}
+		//		}
+
+
+		//		tris[i]._fitness = fitness[i];
+
+
+		//		if (i % 1000 == 0)
+		//		{
+		//			Console.WriteLine($"Evaluating: {i/tris.Length}%");
+		//		}
+		//	}
+
+		//	this.fitness = fitness;
+		//	Array.Sort(fitness, tris);
+		//}
+
 		//make eval that rewards 
 
-		/* code I found, https://stackoverflow.com/questions/11075505/get-all-points-within-a-triangle */
+		/* code I found at https://stackoverflow.com/questions/11075505/get-all-points-within-a-triangle, slightly modified */
 
-		public IEnumerable<Point> PointsInTriangle(Point pt1, Point pt2, Point pt3)
+		public IEnumerable<Point> PointsInTriangle(Point pt1, Point pt2, Point pt3, int granularity)
 		{
 			if (pt1.Y == pt2.Y && pt1.Y == pt3.Y)
 			{
@@ -116,12 +190,12 @@ namespace AI_Art
 			var baseFunc = CreateFunc(pt1, pt3);
 			var line1Func = pt1.X == pt2.X ? (x => pt2.Y) : CreateFunc(pt1, pt2);
 
-			for (var x = pt1.X; x < pt2.X; x++)
+			for (var x = pt1.X; x < pt2.X; x += granularity)
 			{
 				int maxY;
 				int minY = GetRange(line1Func(x), baseFunc(x), out maxY);
 
-				for (var y = minY; y <= maxY; y++)
+				for (var y = minY; y <= maxY; y += granularity)
 				{
 					yield return new Point(x, y);
 				}
@@ -129,12 +203,12 @@ namespace AI_Art
 
 			var line2Func = pt2.X == pt3.X ? (x => pt2.Y) : CreateFunc(pt2, pt3);
 
-			for (var x = pt2.X; x <= pt3.X; x++)
+			for (var x = pt2.X; x <= pt3.X; x += granularity)
 			{
 				int maxY;
 				int minY = GetRange(line2Func(x), baseFunc(x), out maxY);
 
-				for (var y = minY; y <= maxY; y++)
+				for (var y = minY; y <= maxY; y += granularity)
 				{
 					yield return new Point(x, y);
 				}
@@ -169,22 +243,30 @@ namespace AI_Art
 
 		/* end of found code */
 
-		public void Draw(int thisMany)
+		public void Draw(int thisMany, string imageOut)
 		{
 			using (var drawing = Graphics.FromImage(image))
 			{
 				drawing.Clear(Color.White);
 
 				//draws the top ranking thisMany triangles, from worst to best
+				int onePercent = thisMany / 100;
 				for (int i = tris.Length - thisMany; i < tris.Length; i++)
 				{
 					Triangle triangle = tris[i];
 					drawing.FillPolygon(triangle._brush, triangle._points);
+
+					if (i % onePercent == 0)
+					{
+						decimal percent = decimal.Divide(i, tris.Length) * 100;
+						Console.SetCursorPosition(0, 2);
+						Console.WriteLine("Drawing: {0:0}%", percent);
+					}
 				}
 				drawing.Save();
 			}
 
-			image.Save("image.bmp", ImageFormat.Bmp);
+			image.Save(imageOut, ImageFormat.Bmp);
 			image.Dispose();
 		}
 
