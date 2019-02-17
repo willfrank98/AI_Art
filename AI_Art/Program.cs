155f1579bd9165fdcfa31d314e532ada4bfcc965
Will Frank <willfrank98@gmail.com>
2019-02-17 14:11:09 -0700

    implemented DeltaE method for comparing pixel colors

--- AI_Art/Program.cs
+++ AI_Art/Program.cs
@@ -7,129 +7,23 @@ namespace AI_Art
 	{
 		static int Main(string[] args)
 		{
-			string fileIn = "";
-			string fileOut = "";
-			int num = 0;
-			int minLength = 0;
-			int maxLength = 0;
-			int granularity = 0;
-			int drawNum = 0;
-			int seed = 0;
+			string fileIn = "PearlEarring.jpg";
+			TriangleImage image = new TriangleImage(0, fileIn);
 
-			// run with no command line args to be prompted for input
-			if (args.Length == 0)
+			for (int i = 0; i < 1; i++)
 			{
-				Console.WriteLine("Enter input file: ");
-				fileIn = Console.ReadLine();
-				Console.WriteLine("Enter output file: ");
-				fileOut = Console.ReadLine();
-				Console.WriteLine("Enter number of triangles to generate: ");
-				if (!int.TryParse(Console.ReadLine(), out num))
-				{
-					Console.WriteLine("Invalid input!");
-					return 1;
-				}
-				Console.WriteLine("Enter minimum side length: ");
-				if (!int.TryParse(args[3], out minLength))
-				{
-					Console.WriteLine("Invalid input!");
-					return 1;
-				}
-				Console.WriteLine("Enter maximum side length: ");
-				if (!int.TryParse(args[4], out maxLength))
-				{
-					Console.WriteLine("Invalid input!");
-					return 1;
-				}
-				Console.WriteLine("Enter granularity level: ");
-				if (!int.TryParse(args[5], out granularity))
-				{
-					Console.WriteLine("Invalid input!");
-					return 1;
-				}
-				Console.WriteLine("Enter number of triangles to generate: ");
-				if (!int.TryParse(args[6], out drawNum))
-				{
-					Console.WriteLine("Invalid input!");
-					return 1;
-				}
-			}
-			else if (args.Length == 1)
-			{
-				if (string.Equals(args[0], "-hc"))	// -hc to use hardcoded values
-				{
-					args = new string[8];
-					fileIn = "PearlEarring.jpg";		//source file
-					fileOut = "PearlEarring-Out.bmp";	//destination file
-					num = 2000000;						//number to generate
-					minLength = 5;						//min side length
-					maxLength = 30;						//max side length
-					granularity = 3;					//granularity to evaluate at
-					drawNum = 50000;					//number to draw
-					seed = DateTime.Now.Millisecond;	//random seed
-				}
-				else
-				{
-					Console.WriteLine("Invalid Args!");
-				}
-			}
-			else // see README.md for more info about command line args
-			{ 
-				fileIn = args[0];
-				fileOut = args[1];
+				string fileOut = "pearl-out-" + i + ".bmp";
+				int num = 200000;
+				int minLength = 5;
+				int maxLength = 50;
+				int granularity = 3;
+				int drawNum = 50000;
 
-				if (!int.TryParse(args[2], out num))
-				{
-					Console.WriteLine("Invalid Args!");
-					return 1;
-				}
-
-				if (!int.TryParse(args[3], out minLength))
-				{
-					Console.WriteLine("Invalid Args!");
-					return 1;
-				}
-
-				if (!int.TryParse(args[4], out maxLength))
-				{
-					Console.WriteLine("Invalid Args!");
-					return 1;
-				}
-
-				if (!int.TryParse(args[5], out granularity))
-				{
-					Console.WriteLine("Invalid Args!");
-					return 1;
-				}
-
-				if (!int.TryParse(args[6], out drawNum))
-				{
-					Console.WriteLine("Invalid Args!");
-					return 1;
-				}
-
-				seed = args[7].GetHashCode();
+				image.NewBatch(num, minLength, maxLength);
+				image.EvaluateFitness(granularity);
+				image.Draw(drawNum, fileOut);
 			}
 
-			// loads the image to be recreated
-			TriangleImage image = new TriangleImage(seed, fileIn);
-
-			// starts a timer, just for testing
-			// TODO: add option to not time
-			//Stopwatch timer = new Stopwatch();
-			//timer.Start();
-
-			image.NewBatch(num, minLength, maxLength);
-			image.EvaluateFitness(granularity);
-			image.Draw(drawNum, fileOut);
-
-
-
-			//timer.Stop();
-
-			//TimeSpan ts = timer.Elapsed;
-			//string elapsedTime = String.Format("{0:00}:{1:00}:{2:00}.{3:00}", ts.Hours, ts.Minutes, ts.Seconds,	ts.Milliseconds / 10);
-			//Console.WriteLine($"Finished in {elapsedTime}.");
 			return 0;
 		}
 	}
--- AI_Art/TriangleImage.cs
+++ AI_Art/TriangleImage.cs
@@ -12,7 +12,7 @@ namespace AI_Art
 		private int width;
 		private Image image;
 		private Triangle[] tris;
-		private double[] fitness;
+		//private double[] fitness;
 
 		/// <summary>
 		/// Initializer
@@ -74,22 +74,59 @@ namespace AI_Art
 			for (int i = 0; i < tris.Length; i++)
 			{
 				int pixels = 0;
+				double[] avgRGB = { 0, 0, 0 };
 				foreach (Point point in PointsInTriangle(tris[i]._points[0], tris[i]._points[1], tris[i]._points[2], granularity))
 				{
 					if (point.X >= 0 && point.X < image.Width && point.Y >= 0 && point.Y < image.Height)
 					{
 						byte* data = scanImage + point.Y * bData.Stride + point.X * bitsPerPixel / 8;
+						//avgRGB[0] += data[2];
+						//avgRGB[1] += data[1];
+						//avgRGB[2] += data[0];
 
-						//higher fitness = better
 						Color color = ((SolidBrush)tris[i]._brush).Color;
-						fitness[i] += Math.Pow(1.03, 255 - Math.Abs(data[0] - color.B));
-						fitness[i] += Math.Pow(1.03, 255 - Math.Abs(data[1] - color.G));
-						fitness[i] += Math.Pow(1.03, 255 - Math.Abs(data[2] - color.R));
+
+						double[] c1 = { data[2] / 255.0, data[1] / 255.0, data[0] / 255.0 };
+						double[] c2 = { color.R / 255.0, color.G / 255.0, color.B / 255.0 };
+
+						// convert RGB to XYZ
+						c1 = RGBtoXYZ(c1);
+						c2 = RGBtoXYZ(c2);
+
+						// XYZ to Lab
+						c1 = XYZtoLab(c1);
+						c2 = XYZtoLab(c2);
+
+						// Delta E (CIE 1976)
+						double E = Math.Sqrt(Math.Pow(c1[0] - c2[0], 2) + Math.Pow(c1[1] - c2[1], 2) + Math.Pow(c1[2] - c2[2], 2));
+						fitness[i] += E;
+
 						pixels++;
 					}
 				}
 
-				fitness[i] = fitness[i] / Math.Pow(pixels, .5);
+				//avgRGB[0] /= pixels * 255;
+				//avgRGB[1] /= pixels * 255;
+				//avgRGB[2] /= pixels * 255;
+
+				//Color color = ((SolidBrush)tris[i]._brush).Color;
+				//double[] c2 = { color.R / 255.0, color.G / 255.0, color.B / 255.0 };
+
+				//// convert RGB to XYZ
+				//avgRGB = RGBtoXYZ(avgRGB);
+				//c2 = RGBtoXYZ(c2);
+
+				//// XYZ to Lab
+				//avgRGB = XYZtoLab(avgRGB);
+				//c2 = XYZtoLab(c2);
+
+				//// Delta E (CIE 1976)
+				//double E = Math.Sqrt(Math.Pow(avgRGB[0] - c2[0], 2) + Math.Pow(avgRGB[1] - c2[1], 2) + Math.Pow(avgRGB[2] - c2[2], 2));
+
+				fitness[i] /= pixels;
+				fitness[i] = 1/fitness[i];
+
+				//fitness[i] = fitness[i] / pixels;
 
 				if (i % onePercent == 0)
 				{
@@ -99,8 +136,71 @@ namespace AI_Art
 				}
 			}
 
-			this.fitness = fitness;
+			//this.fitness = fitness;
 			Array.Sort(fitness, tris);
+		}
+
+		private double[] RGBtoXYZ(double[] c)
+		{
+			for (int i = 0; i < c.Length; i++)
+			{
+				if (c[i] <= 0.0405)
+				{
+					c[i] = c[i] / 12.92;
+				}
+				else
+				{
+					c[i] = Math.Pow((c[i] + 0.055) / 1.055, 2.4);
+				}
+				//c[i] = Math.Pow(c[i], 2.2);
+			}
+
+			double[,] M = new double[3, 3]{ { 0.4124564, 0.3575761, 0.1804375 }, 
+											{ 0.2126729, 0.7151522, 0.0721750 }, 
+											{ 0.0193339, 0.1191920, 0.9503041 } };
+
+			double[] newC = new double[c.Length];
+			for (int i = 0; i < 3; i++)
+			{
+				double temp = 0;
+				for (int j = 0; j < 3; j++)
+				{
+					temp += c[j] * M[i, j];
+				}
+				newC[i] = temp;
+			}
+
+			return newC;
+		}
+
+		private double[] XYZtoLab(double[] c)
+		{
+			double[] white = { 0.95047, 1.0000001, 1.08883 };
+
+			double epsilon = 216 / 24389.0;
+			double kappa = 24389 / 27.0;
+
+			double[] r = new double[3];
+			for (int i = 0; i < r.Length; i++)
+			{
+				r[i] = c[i] / white[i];
+			}
+
+			double[] f = new double[3];
+			for (int i = 0; i < f.Length; i++)
+			{
+				if (r[i] > epsilon)
+				{
+					f[i] = Math.Pow(r[i], 1.0 / 3.0);
+				}
+				else
+				{
+					f[i] = (r[i] * kappa + 16) / 116;
+				}
+			}
+
+			double[] Lab = { 116 * f[1] - 16, 500 * (f[0] - f[1]), 200 * (f[1] - f[2]) };
+			return Lab;
 		}
 
 
@@ -223,7 +323,7 @@ namespace AI_Art
 			}
 
 			image.Save(imageOut, ImageFormat.Bmp);
-			image.Dispose();
+			//image.Dispose();
 		}
 
 		// gets the bits per pixel, used for pointer math in images
